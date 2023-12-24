c2f9e0b24d11e536327162762a1e4e89c55c921f
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2023-12-25 00:06:57 +0100

    send

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { FaPaperPlane } from "react-icons/fa";
-
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
 export default function SubmitBtn() {
-
+  const { pending } = useFormStatus();
 
   return (
     <button
       type="submit"
-      className="group   flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
-      style={{ zIndex: 1}}
+      className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
+      disabled={pending}
     >
-     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-</svg>
-
+      {pending ? (
+        <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
+      ) : (
+        <>
+          Send
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+        </>
+      )}
     </button>
   );
 }

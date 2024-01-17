945fedfe043fc088f5faea08aa759df34a92c21f
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-01-17 23:02:48 +0100

    b

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,6 @@ const config: Config = {
     extend: {
       animation: {
         "text-reveal": "text-reveal 1.5s cubic-bezier(0.77, 0, 0.175, 1) 0.5s",
-      }, components: {
-        backgroundImage : {
-          'gradient-bg':'linear-gradient(241deg, #309A6D 22.4%,    #7DC767 69.51%, #F9F952 95.88%)',
-        },
       },
       keyframes: {
         "text-reveal": {

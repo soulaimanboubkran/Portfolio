d16ced0f2be02456a0e121f4291b2f594f1e1bb3
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-01-18 20:06:46 +0100

    er

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,12 +31,12 @@ export default function RootLayout({
   className="absolute z-[-12] inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:33px_33px]"
 >
 
-
-<p className="bg-[#195190FF] absolute top-[-6rem]   md:right-[1rem] right-[1rem] h-[66.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#7e393a]"></p>
-<p className="bg-[#676394] absolute top-[22rem]  left-[-35rem] h-[99.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-11rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></p>
-<p className="bg-[#195190FF] absolute bottom-[-6rem]   md:right-[1rem] right-[1rem] h-[50.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#7e393a]"></p>
-<p className="bg-[#ABD1C9FF] absolute bottom-[22rem]  left-[-35rem] h-[44.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-11rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></p>
-      
+<div className="relative">
+  <div className="bg-[#195190FF] absolute top-[-6rem] md:right-[1rem] right-[1rem] h-[66.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#7e393a]"></div>
+  <div className="bg-[#676394] absolute top-[22rem] left-[-35rem] h-[99.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-11rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+  <div className="bg-[#195190FF] absolute bottom-[-6rem] md:right-[1rem] right-[1rem] h-[50.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#7e393a]"></div>
+  <div className="bg-[#ABD1C9FF] absolute bottom-[22rem] left-[-35rem] h-[44.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-11rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+</div>   
 </div>
             {children}
          <SpeedInsights/>

6cb37daca36e1bddd7dafb64b578728a1b65666c
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-02-25 21:24:26 +0100

    commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className=" px-4 text-center text-gray-500">
+    <footer className="mb-8 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
         &copy; 2023 Soulaimane. All rights reserved.
       </small>

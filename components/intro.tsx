3886491237baef9ab78d4807dfb26feaad6b383f
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-07-17 00:17:42 +0100

    commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -87,7 +87,7 @@ const Intro = () => {
     </Link>
     <a
       className="group bg-white px-7 z-20 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-      href="/CV.pdf"
+      href="/SoulaimaneBouiboukirn.pdf"
       download
     >
       Download CV{" "}

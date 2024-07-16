84e26717f1a0ca4ce274e8d5971ef4b18a8f857b
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-07-16 01:19:20 +0100

    commit

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -4,14 +4,14 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import type { SectionName } from "./types";
 
-export function useSectionInView(sectionName: SectionName, threshold = 0.2) {
+export function useSectionInView(sectionName: SectionName, threshold = 0.1) {
   const { ref, inView } = useInView({
     threshold,
   });
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
   useEffect(() => {
-    if (inView && Date.now() - timeOfLastClick > 2000) {
+    if (inView && Date.now() - timeOfLastClick > 100) {
       setActiveSection(sectionName);
     }
   }, [inView, setActiveSection, timeOfLastClick, sectionName]);

03326eb51157d030fd31a1bf7ac7556e227a4bac
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-08-16 21:31:54 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'avatars.githubusercontent.com',
-            
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
       },
-     
-}
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/CV.pdf',
+        destination: '/SoulaimaneBouiboukirn.pdf',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
